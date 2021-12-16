1fa81f2ac96c55be89b30b4bd6bc1cfd8c2dce47
Colin Delahunty <72827203+colin99d@users.noreply.github.com>
2021-12-16 12:27:20 -0500

    Add files via upload

---
+++ create_data.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+// a struct to read and write
+struct person
+{
+    char fname[64];
+    char lname[64];
+    float balance;
+};
+ 
+int main ()
+{
+    FILE *outfile;
+     
+    // open file for writing
+    outfile = fopen ("person.dat", "w");
+    if (outfile == NULL)
+    {
+        fprintf(stderr, "\nError opened file\n");
+        exit (1);
+    }
+ 
+    struct person input1 = {"rohan", "sharma", 100.00};
+    struct person input2 = {"mahendra", "dhoni", 100.00};
+     
+    // write struct to file
+    fwrite (&input1, sizeof(struct person), 1, outfile);
+    fwrite (&input2, sizeof(struct person), 1, outfile);
+ 
+    // close file
+    fclose (outfile);
+ 
+    return 0;
+}
